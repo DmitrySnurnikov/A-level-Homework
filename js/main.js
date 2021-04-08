e6518e33369728fe20e53a90348f269e92d029f7
Дима Снурников <snurnikovdima@gmail.com>
2021-04-08 07:40:57 +0300

    new repoz

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Task 1
+
+// document.addEventListener('keydown', function(event){
+//   if(event.code === 'KeyP'){
+//     console.log('Hello, world!');
+//   }
+// });
+
+
+// Task 2
+
+// document.addEventListener('keydown', function(event){
+//   if(event.ctrlKey & event.code === 'KeyP'){
+//     console.log('Hello, world!');
+//   }
+// });
