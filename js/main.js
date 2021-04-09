6397a7fe331f8f25363404afc394eaa2411e8d66
Дима Снурников <snurnikovdima@gmail.com>
2021-04-09 10:16:03 +0300

    added new dz

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 // Task 1
 
-// document.addEventListener('keydown', function(event){
-//   if(event.code === 'KeyP'){
-//     console.log('Hello, world!');
-//   }
-// });
+document.addEventListener('keydown', function(event){
+  if(event.code === 'KeyP'){
+    console.log('Hello, world!');
+  }
+});
 
 
 // Task 2
 
-// document.addEventListener('keydown', function(event){
-//   if(event.ctrlKey & event.code === 'KeyP'){
-//     console.log('Hello, world!');
-//   }
-// });
+function addListener(type,handler){
+  var keys = type.split('+');
+  console.log(keys);
+  document.addEventListener('keydown', function(event){
+      if(event.code === keys[0]){
+        alert(Hello);
+        keys.shift();
+    };
+    if(keys.length === 0){
+      keys = source.slice(0);
+    }
+    console.log(event);
+  });
+};
+addListener('Control+Y',12)
